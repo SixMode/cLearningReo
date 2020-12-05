d1f491a2e9cc30fa43840f5cfdd607e2f383dfa6
Zac Childress <60277286+SixMode@users.noreply.github.com>
2020-12-04 23:56:51 -0600

    Add files via upload
    Exercise 1-12 from "The C Programming Language"

---
+++ exercise1-12/exercise1-12/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int c;
+    
+    while ((c = getchar()) != EOF)
+    {
+        if (c == ' ')
+        {
+            c = '\n';
+        }
+        putchar(c);
+    }
+    return 0;
+}
